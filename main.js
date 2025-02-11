2ca69708714824593348385a0d7ad44e49957f8d
khushiiiii24 <khushisogani24@gmail.com>
2025-02-11 15:17:39 +0530

    second commit

--- main.js
+++ main.js
@@ -1,48 +1,36 @@
 import questions from "./questions.js";
 
 const btn = document.querySelector("button");
-const quizSection = document.querySelector(".quizSection");
+const actualQuizSection = document.querySelector(".actualQuizSection");
 const timerDiv = document.querySelector(".time");
-// btn.addEventListener("click", () => {
-//     setTimeout(() => console.log("Khushi"), 2000)
-// }
+const ques = document.querySelector(".ques");
+const queopt = document.querySelector(".queopt");
+const userAnswers = []
 
-// function queAns() {
-//     questions.map((object) => {
-//         const heading = document.createElement("h1");
-//         // const opt = document.createElement("p");
-//         heading.innerHTML = object.q;
-//         quizSection.append(heading);
-//         object.options.map((opt) => {
-//             const option = document.createElement("p");
-//             option.innerHTML = opt;
-//             quizSection.append(option);
-//         })
-//     })
-// }
 
 let queCount = 0;
-let timer = 2;
+let timer = 5;
 btn.addEventListener("click", () => {
     btn.style.display = "none";
-
     //for pring the first que instantly
     timerDiv.innerHTML = timer;
+    timerDiv.style.cssText="background-color:black;color:white;width:30px;height:30px;border-radius:50%;display:flex;justify-content:center;align-items:center";
     displayQuestionAndOptions(queCount);
-    quizSection.classList.add("bg-white","rounded-lg");
-    timerDiv.classList.add("bg-white","rounded-lg");
+    // actualQuizSection.classList.add("bg-white","rounded-lg");
+    // timerDiv.classList.add("bg-white","rounded-lg");
     const interval = setInterval(() => {
         if (timer === 0) {
             if (queCount < questions.length - 1) {
                 queCount++
-                timer = 2;
+                timer = 5;
                 timerDiv.innerHTML = timer;
                 displayQuestionAndOptions(queCount);
             }
             else {
                 clearInterval(interval);
-                quizSection.innerHTML = "";
-                timerDiv.innerHTML="";
+                actualQuizSection.innerHTML = "";
+                timerDiv.innerHTML = "";
+                calculateScore()
             }
         }
         else {
@@ -54,28 +42,109 @@ btn.addEventListener("click", () => {
 });
 
 function displayQuestionAndOptions(queCount) {
-    quizSection.innerHTML = ""
+    ques.innerHTML = "";
+    queopt.innerHTML = "";
     const heading = document.createElement("h1");
-    // const opt = document.createElement("p");
+
     heading.innerHTML = questions[queCount].q;
-    quizSection.append(heading);
+    ques.append(heading);
+    actualQuizSection.append(ques);
 
     questions[queCount].options.map((opt) => {
         const option = document.createElement("p");
+        option.classList.add("w-[46%]", "my-4", "bg-black", "text-white", "border-2", "rounded-lg", "px-4", "py-1", "cursor-pointer", "hover:bg-white", "hover:text-black", "capitalize")
         option.innerHTML = opt;
-        quizSection.append(option);
+        option.addEventListener("click", storeUserAnswer);
+        queopt.append(option);
+        actualQuizSection.append(queopt);
     })
 
 }
-// let count = 0;
+
+function storeUserAnswer(e) {
+    userAnswers.push(e.target.innerText);
+}
+
+function calculateScore() {
+    // console.log("calculating score");
+    let score = 0
+    userAnswers.forEach((answer, index) => {
+        if (answer === questions[index].a) score++;
+    })
+    console.log("Your score is: " + score);
+    
+
+}
+
+
+
+
+
+// // let count = 0;
+// // btn.addEventListener("click", () => {
+// //     const interval = setInterval(() => {
+// //         if (count === 5) clearInterval(interval);
+// //         else {
+// //             console.log("Khushi");
+// //             count++;
+// //         }
+// //     }, 1000);
+// // });
+
+// // queAns();
+
+
+
+
+
+// import questions from "./questions.js";
+
+// const btn = document.querySelector("button");
+// const actualQuizSection = document.querySelector(".actualQuizSection");
+// const timerDiv = document.querySelector(".time");
+
+// let queCount = 0;
+// let timer = 2;
+
 // btn.addEventListener("click", () => {
+//     btn.style.display = "none";
+
+//     timerDiv.innerHTML = timer;
+//     displayQuestionAndOptions(queCount);
+
 //     const interval = setInterval(() => {
-//         if (count === 5) clearInterval(interval);
-//         else {
-//             console.log("Khushi");
-//             count++;
+//         if (timer === 0) {
+//             if (queCount < questions.length - 1) {
+//                 queCount++;
+//                 timer = 2;
+//                 timerDiv.innerHTML = timer;
+//                 displayQuestionAndOptions(queCount);
+//             } else {
+//                 clearInterval(interval);
+//                 actualQuizSection.innerHTML = "";
+//                 timerDiv.innerHTML = "";
+//             }
+//         } else {
+//             timer--;
+//             timerDiv.innerHTML = timer;
 //         }
 //     }, 1000);
 // });
 
-// queAns();
+// function displayQuestionAndOptions(queCount) {
+//     actualQuizSection.innerHTML = "";
+
+//     const heading = document.createElement("h1");
+//     heading.innerHTML = questions[queCount].q;
+//     actualQuizSection.append(heading);
+
+//     const queopt = document.createElement("div");
+
+//     questions[queCount].options.forEach((opt) => {
+//         const option = document.createElement("p");
+//         option.innerHTML = opt;
+//         queopt.append(option);
+//     });
+
+//     actualQuizSection.append(queopt);
+// }
--- questions.js
+++ questions.js
@@ -1,13 +1,25 @@
 const questions = [
     {
-        q: "what is the capital of India",
+        q: "what is the capital of India?",
         a: "New Delhi",
         options: ["Mumbai", "New Delhi", "kolkata", "chennai"]
     }, {
-        q: "who is the president of India",
+        q: "who is the president of India?",
         a: "Draupadi Murmu",
-        options: ["Narendra modi", "Rahul Gnadhi", "Draupadi Murmu"]
-    }
+        options: ["Narendra modi", "Rahul Gandhi", "Draupadi Murmu", "Arvindra Kejriwal"]
+    }, {
+        q: "Where was India’s first national Museum opened?",
+        a: "Mumbai",
+        options: ["Delhi", "Hyderabad", "Rajasthan", "Mumbai"]
+    }, {
+        q: " In 2019, Which popular singer was awarded the Bharat Ratna award?",
+        a: "Bhupen Hazarika",
+        options: ["Lata Mangeshkar", "Asha Bhosle", " Bhupen Hazarika ", "Manna Dey"]
+    }, {
+        q: "The world’s nation 5G mobile network was launched by which country?",
+        a: "South Korea",
+        options: ["Japan", "Asia", , "South Korea", "Malaysia"]
+    },
 ]
 
 export default questions;
